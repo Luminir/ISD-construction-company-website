3300a7a9bd280457296b3d3bd318b3dceada0a44
Tran Nam Son <trannamson0328@gmail.com>
2025-03-18 21:02:34 +0700

    implement about, update home and nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +30,7 @@ function App() {
 // SƠn: e dã tạo home.jsx trong pages/
 // const Home = () => <div className="pt-20 bg-amber-300 text-black text-center text-xl">Home Page</div>;
 // const Contact = () => <div className="pt-20 bg-amber-300 text-black text-center text-xl">Contact Page</div>;
-const About = () => <div className="pt-20 bg-[#1f377e] text-white text-center text-xl h-200">About Page</div>;
+// const About = () => <div className="pt-20 bg-[#1f377e] text-white text-center text-xl h-200">About Page</div>;
 const Services = () => <div className="pt-20 bg-[#1f377e] text-white text-center text-xl h-200">Services Page</div>;
 const Project = () => <div className="pt-20 bg-[#1f377e] text-white text-center text-xl h-200">Project Page</div>;
 const News = () => <div className="pt-20 bg-[#1f377e] text-white text-center text-xl h-200">News Page</div>;
